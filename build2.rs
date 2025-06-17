b689aa8bd77353b37a6f5ac7e7ebf5a467ab3cb5
Naman Garg <ngarg1270@gmail.com>
2025-06-17 23:59:07 +0530

    Split into 2 scripts

---
+++ build2.rs
@@ -0,0 +1,9 @@
+fn main(){
+    cc::Build::new()
+        .files(&["src/scanning_and_printing.c"])
+        .compile("c_lib");
+    // cc::Build::new()
+    //     .cpp(true)
+    //     .files(&["src/my_gcd.cpp"])
+    //     .compile("cpp_lib");
+}
