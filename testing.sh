1e7c3dbc5c0166e62780ea2877834cc542a52703
Naman Garg <ngarg1270@gmail.com>
2025-07-10 23:22:30 +0530

    Increase n

--- testing.sh
+++ testing.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-for n in {1..10}; 
+for n in {1..15}; 
 do
    rustup run nightly ../cargo/target/release/cargo clean
    echo $n $n | rustup run nightly ../cargo/target/release/cargo run
    echo $n
 done
-
