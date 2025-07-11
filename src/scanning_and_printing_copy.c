49fe6ff4171d78d59fb2f54ce7a338bff707f622
Naman Garg <ngarg1270@gmail.com>
2025-07-11 23:34:37 +0530

    Minor updates

--- src/scanning_and_printing_copy.c
+++ src/scanning_and_printing_copy.c
@@ -2,7 +2,7 @@
 
 int scan_and_print() {
     int num;
-    printf("OK! Enter the copy integer: ");
+    printf("OK! Enter the copy of the integer: ");
     scanf("%d", &num);
     printf("You copied: %d\n", num);
     return num;
